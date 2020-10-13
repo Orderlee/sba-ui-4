facf2656d2a96086720a888c4de8769e14dfb6c1
orderlee <nuncateolvidar3@gmail.com>
2020-10-13 11:53:00 +0900

    commint on 2

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,7 +3,20 @@ import {About,Contact,Events,Home,Products,Error} from '../components'
 import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 
 const Main = () => <>
-
-</>
+        <BrowserRouter>
+            <div className="main">
+                <switch>
+                    <Route exct path="/" component={Home}/>
+                    <Route path="/about" component={About}/>
+                    <Redirect from={"/history"} to={"/about/history"}/>
+                    <Route path="contact" component={Contact}/>
+                    <Route path="/events" component={Events}/>
+                    <Route path="/products" component={Products}/>
+                    <Route component={Error}/>
+                    
+                </switch>
+            </div>
+        </BrowserRouter>
+    </>
 
 export default Main
